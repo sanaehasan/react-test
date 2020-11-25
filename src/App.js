bba399f0efd0bcbc950fbf985447c79c0ec832f9
sanae <sanae.k1986@gmail.com>
2020-11-25 11:45:39 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -2,32 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class Add extends React.Component{
+// class Add extends React.Component{
 
-   render(){
-     return <h1>{this.props.a+this.props.b}</h1>;
-   }
-}
+//    render(){
+//      return <h1>{this.props.a+this.props.b}</h1>;
+//    }
+// }
 
-const Layout = props => {
-  return (
-  <React.Fragment>
-    <header>my header</header>
-    <main>{props.children}</main>
-    <footer>footer</footer>
-  </React.Fragment>
-  );
-}
+// const Layout = props => {
+//   return (
+//   <React.Fragment>
+//     <header>my header</header>
+//     <main>{props.children}</main>
+//     <footer>footer</footer>
+//   </React.Fragment>
+//   );
+// }
 
 function App() {
 
   return (
-    <div>
-   <Layout>
-   <p>hjhskdsakjk</p>
-   <Add a={1} b={2}/>
-   </Layout>
-   </div>
+  <button onClick={()=> console.log(hi)}>tell me a jocke</button>
   );
 }
 
